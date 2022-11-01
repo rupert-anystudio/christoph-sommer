239ca471f4dd84047fdeaff9ed0f452e44c59053
Rupert Adlmaier <rupert@any.studio>
2022-11-01 15:17:41 +0100

    pull about data into page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { getClient } from '../lib/sanity.server'
 import CardGridPortfolio from '../components/CardGridPortfolio'
 import { entryQuery } from '../lib/entryHelpers'
 
-export default function Home({ docs = [] }) {
+export default function Home({ docs = [], about = {} }) {
   return (
     <>
       <CardGridPortfolio portfolio={docs} />
@@ -12,13 +12,18 @@ export default function Home({ docs = [] }) {
 
 export async function getStaticProps({ preview = false }) {
   const client = getClient(preview)
+  // fetch all docs for cards
   const docsQuery = `*[_type in ["publishedText", "project", "statement", "speech"]]|order(_createdAt desc)${entryQuery}`
   const docs = await client.fetch(docsQuery)
+  // fetch about content
+  const aboutQuery = `*[_type == "aboutPage"]{ aboutText, missionStatement }`
+  const about = await client.fetch(aboutQuery)
   return {
     revalidate: 10,
     props: {
       preview,
       docs,
+      about,
     },
   }
 }

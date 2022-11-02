0005103f9fcf82aca22334e0a70255b729d9b3c3
Rupert Adlmaier <rupert@any.studio>
2022-11-03 00:48:34 +0100

    setting up next for gsap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const withTM = require('next-transpile-modules')(['gsap'])
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,4 +8,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)

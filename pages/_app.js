48558aa2e5797d73cd631011d83906391aa404f3
Rupert Adlmaier <rupert@any.studio>
2022-11-04 14:58:36 +0100

    add gsap to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'normalize.css'
+import { gsap } from 'gsap'
+import { CSSPlugin } from 'gsap/CSSPlugin'
 import Annoucements from '../components/Annoucements'
 import { ThemeContextProvider } from '../components/ThemeContext'
 import { FilterContextProvider } from '../components/FilterContext'
@@ -10,6 +12,8 @@ import Logo from '../components/Logo'
 import Main from '../components/Main'
 import { PagePropsContextProvider } from '../components/PagePropsContext'
 import ThemeToggle from '../components/ThemeToggle'
+
+gsap.registerPlugin(CSSPlugin)
 
 function MyApp({ Component, pageProps }) {
   return (

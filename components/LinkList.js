d3f01d2780eedbeb8efc409619a4a0cdcd0a84a2
Rupert Adlmaier <rupert@any.studio>
2022-12-16 10:11:41 +0100

    Linklist

--- components/LinkList.js
+++ components/LinkList.js
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import styled from 'styled-components'
 import ExternalLink from './ExternalLink'
 import { Link } from './Primitives'
@@ -14,28 +15,42 @@ const Entry = styled(Link).attrs({ as: 'li' })`
   }
 `
 
-const LinkList = ({ entries = [], isDisabled }) => {
-  if (!entries) return null
-  if (entries.length < 1) return null
-  return (
-    <List>
-      {entries.map((entry) => {
-        const { key, label, href } = entry
-        return (
-          <Entry key={key}>
-            <ExternalLink
-              href={href}
-              // staticIcon={'·'}
-              activeIcon={'↗'}
-              isDisabled={isDisabled}
-            >
-              {label}
-            </ExternalLink>
-          </Entry>
-        )
-      })}
-    </List>
-  )
-}
+const LinkList = forwardRef(
+  (
+    {
+      entries = [],
+      isDisabled,
+      staticIcon,
+      activeIcon = '↗',
+      className,
+      style,
+    },
+    ref
+  ) => {
+    if (!entries) return null
+    if (entries.length < 1) return null
+    return (
+      <List ref={ref} className={className} style={style}>
+        {entries.map((entry) => {
+          const { key, label, href } = entry
+          return (
+            <Entry key={key}>
+              <ExternalLink
+                href={href}
+                staticIcon={staticIcon}
+                activeIcon={activeIcon}
+                isDisabled={isDisabled}
+              >
+                {label}
+              </ExternalLink>
+            </Entry>
+          )
+        })}
+      </List>
+    )
+  }
+)
+
+LinkList.displayName = 'LinkList'
 
 export default LinkList

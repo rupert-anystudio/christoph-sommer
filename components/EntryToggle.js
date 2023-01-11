60f929a469729b628dcbe7bd9d345820a33d99b3
Rupert Adlmaier <rupert@any.studio>
2023-01-11 16:59:05 +0100

    hide when open

--- components/EntryToggle.js
+++ components/EntryToggle.js
@@ -64,7 +64,8 @@ const Wrap = styled.div`
   @media (hover: hover) {
     > button > div {
       transition: transform 0.3s ease-in-out;
-      transform: translate3d(0, ${(p) => (p.isSelected ? 0 : '100%')}, 0);
+      /* transform: translate3d(0, ${(p) => (p.isSelected ? 0 : '100%')}, 0); */
+      transform: translate3d(0, 100%, 0);
     }
     &:hover {
       > button > div {
